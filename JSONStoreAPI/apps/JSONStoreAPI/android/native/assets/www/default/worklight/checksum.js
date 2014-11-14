f786c0d4a48e64383c6a303666bb778cbeb8a1b2
JeffTee7 <278523281@qq.com>
2014-11-14 22:38:00 +0800

    test

---
+++ JSONStoreAPI/apps/JSONStoreAPI/android/native/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":3867111553,"date":1408441287441,"machine":"ADMINIB-PN2VGJ9"};
+/* Date: Tue Aug 19 17:41:27 CST 2014 */
---
+++ JSONStoreAPI/apps/JSONStoreAPI/android/native/src/com/JSONStoreAPI/GCMIntentService.java
@@ -0,0 +1,5 @@
+package com.JSONStoreAPI;
+
+public class GCMIntentService extends com.worklight.androidgap.push.GCMIntentService{
+	//Nothing to do here...
+}
